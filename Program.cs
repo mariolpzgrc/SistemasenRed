4d8ffc06e1c8d871090b6c469ec647140d2f60e1
mariolpzgrc <lunix_king@hotmail.com>
2018-08-21 16:51:43 -0500

    Commit inicial. Se agrega gitignore

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Actividad001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Esto es un programa  de C# corriendo en Debian");
+        }
+    }
+}
